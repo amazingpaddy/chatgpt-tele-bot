3b1905e2b01e051decaa0c81bbd1805cd8ec1fff
Paddy <padmanabhan5789@gmail.com>
2023-02-24 10:38:39 -0500

    Added exception decorator

--- openai-chat-tbot.py
+++ openai-chat-tbot.py
@@ -5,6 +5,7 @@ import os
 import requests
 from telebot.async_telebot import AsyncTeleBot
 from flask import Flask, request
+import traceback
 
 # Get OpenAI API key, Telegram bot token, bot personality, and authorized users from environment variables
 API_KEY = os.environ.get('OPENAI_API_KEY')
@@ -21,6 +22,28 @@ bot = AsyncTeleBot(BOT_TOKEN)
 # Create a Flask app object with the current module name
 app = Flask(__name__)
 
+
+def catch_and_print_exception(func):
+    """
+    A decorator that catches exceptions and prints the stack trace.
+
+    Args:
+        func: The function to be decorated.
+
+    Returns:
+        The decorated function.
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            traceback.print_exc()
+
+    return wrapper
+
+
+@catch_and_print_exception
 @app.route(f'/{BOT_TOKEN}', methods=['POST'])
 async def handle_update():
     """
@@ -40,6 +63,7 @@ async def handle_update():
     return 'ok'
 
 
+@catch_and_print_exception
 async def openAI(prompt):
     """
     Make a request to the OpenAI API and return the response.
@@ -63,6 +87,7 @@ async def openAI(prompt):
     return final_result
 
 
+@catch_and_print_exception
 @bot.message_handler(commands=['ask'])
 async def ask(message):
     """
@@ -71,33 +96,29 @@ async def ask(message):
     Check if the user is authorized to use the bot, extract the query from the message,
     pass it to the openAI function for processing, and send the response back to the user.
     """
-    try:
-        # Get the username of the message sender
-        username = message.from_user.username
+    # Get the username of the message sender
+    username = message.from_user.username
 
-        # Check if the user is authorized to use the bot
-        if username not in AUTHORIZED_USERS:
-            await bot.reply_to(message, "Not authorized to use this bot")
-            return
+    # Check if the user is authorized to use the bot
+    if username not in AUTHORIZED_USERS:
+        await bot.reply_to(message, "Not authorized to use this bot")
+        return
 
-        # Extract the query from the message
-        prompt = message.text.replace("/ask", "").strip()
+    # Extract the query from the message
+    prompt = message.text.replace("/ask", "").strip()
 
-        # If the query is empty, send an error message to the user
-        if not prompt:
-            await bot.reply_to(message, "Empty query sent. Add your query /ask <message>")
-        else:
-            # Send the query to the OpenAI API and get the response
-            bot_response = await openAI(f"{BOT_PERSONALITY}{prompt}")
+    # If the query is empty, send an error message to the user
+    if not prompt:
+        await bot.reply_to(message, "Empty query sent. Add your query /ask <message>")
+    else:
+        # Send the query to the OpenAI API and get the response
+        bot_response = await openAI(f"{BOT_PERSONALITY}{prompt}")
 
-            # Send the response back to the user
-            await bot.reply_to(message, bot_response.replace('?\n\n', ''))
-
-    except Exception as e:
-        print("Exception happened")
-        print(e)
+        # Send the response back to the user
+        await bot.reply_to(message, bot_response.replace('?\n\n', ''))
 
 
+@catch_and_print_exception
 @bot.message_handler(commands=['start'])
 async def start(message):
     """
@@ -109,26 +130,21 @@ async def start(message):
     Returns:
         None
     """
-    try:
-        # Get the username of the user who sent the '/start' command
-        username = message.from_user.username
-        # Create the welcome message
-        result = f"""
-        Welcome {username}!!, With this bot, you can send your queries to ChatGPT and receive responses. 
-        To post your questions, use the "/ask <message>" format. 
-        For example, type "/ask Explain like I'm 5, what is entropy?"
-        Please allow 1-2 minutes for a reply. 
-        If you don't receive a response, try again later. 
-        Note that the bot does not retain previous conversations, so follow-up questions 
-        may not receive the expected answer from ChatGPT.
-        Thanks for using this bot - Paddy!!
-        """
-        # Send the welcome message to the user
-        await bot.send_message(message.chat.id, result)
-    except Exception as e:
-        # If an exception occurs, print the error message to the console
-        print("Exception happened")
-        print(e)
+    # Get the username of the user who sent the '/start' command
+    username = message.from_user.username
+    # Create the welcome message
+    result = f"""
+    Welcome {username}!!, With this bot, you can send your queries to ChatGPT and receive responses. 
+    To post your questions, use the "/ask <message>" format. 
+    For example, type "/ask Explain like I'm 5, what is entropy?"
+    Please allow 1-2 minutes for a reply. 
+    If you don't receive a response, try again later. 
+    Note that the bot does not retain previous conversations, so follow-up questions 
+    may not receive the expected answer from ChatGPT.
+    Thanks for using this bot - Paddy!!
+    """
+    # Send the welcome message to the user
+    await bot.send_message(message.chat.id, result)
 
 
 # Get the port number from an environment variable or use 5000 as default
